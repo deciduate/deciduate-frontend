618b9e21509c71e06583c44704f36d9ae1b68256
choigyulee <cgl1409@gmail.com>
2024-05-27 23:17:49 +0900

    Docs: 문서 수정

--- graduateapp/src/App.js
+++ graduateapp/src/App.js
@@ -12,7 +12,7 @@ import EditSubjects from "./component/My/EditSubjects";
 import EditExtras from "./component/My/EditExtras";
 import ViewMy from './component/My/ViewMy';
 import Result_NonPass from './component/Result/Result_NonPass';
-// import Result_Pass from './component/Result/Result_Pass';
+import Result_Pass from './component/Result/Result_Pass';
 
 function App() {
   return (
@@ -26,12 +26,12 @@ function App() {
           <Route path="/MY/EditCredits" element={<EditCredits />} />
           <Route path="/MY/EditSubjects" element={<EditSubjects />} />
           <Route path="/MY/EditExtras" element={<EditExtras />} />
-          {/* <Route path="/MY/Loading" element={<Loading />} /> */}
+          <Route path="/MY/Loading" element={<Loading />} />
           <Route path="/InsertBasic" element={<InsertBasic />} />
           <Route path="/StepInsert" element={<StepInsert />} />
           <Route path="/MY/ViewMy" element={<ViewMy />} />
           <Route path='/MY/Result_NonPass' element={<Result_NonPass/>}/>
-          {/* <Route path='/MY/Result_Pass' element={<Result_Pass/>}/> */}
+          <Route path='/MY/Result_Pass' element={<Result_Pass/>}/>
         </Routes>
       </BrowserRouter>
     </>
